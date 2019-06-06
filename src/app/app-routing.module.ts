808414dd58bf91638e7c78761f74d7dc12545e56
evillarbarrera <e.villarbarrera@gmai.com>
2019-06-05 20:37:01 -0400

    change css header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'head', loadChildren: './head/head.module#HeadPageModule' },
-  { path: 'footer', loadChildren: './footer/footer.module#FooterPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
 ];
 
 @NgModule({
