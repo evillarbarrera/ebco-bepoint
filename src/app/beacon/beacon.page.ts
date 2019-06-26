7979bf1e5d2b3f3233430401396a141430dcd64f
evillarbarrera <e.villarbarrera@gmai.com>
2019-06-25 23:14:27 -0400

    change beacons

--- src/app/beacon/beacon.page.ts
+++ src/app/beacon/beacon.page.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectorRef } from "@angular/core";
 import { Platform } from '@ionic/angular';
 
+
 @Component({
   selector: "app-beacon",
   templateUrl: "./beacon.page.html",
@@ -10,17 +11,21 @@ export class BeaconPage  {
   beaconData: any;
   constructor(private change: ChangeDetectorRef, private platform: Platform) {}
 
-
   Buscarbeacons(){
+    alert("Buscando...")
     this.platform.ready().then(() =>{
+      alert("Buscando2...")
       evothings.eddystone.startScan((data) => {
        this.beaconData=data;
-     
+       alert("Buscando3...," + this.beaconData.address)
        setTimeout(()=>{
          this.change.detectChanges();
-       }, 3000);
-      }, error => console.error(error));
+       }, 1000);
+      }, error => console.error( alert("Error...")));
     })
   }
+
+
+
 
 }
