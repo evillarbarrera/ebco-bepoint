ba3bdfc4955a145af8260d97ef94df683dcba3dd
evillarbarrera <e.villarbarrera@gmai.com>
2019-06-25 22:32:53 -0400

    test beacons
    Signed-off-by: evillarbarrera <e.villarbarrera@gmai.com>

--- src/app/beacon/beacon.page.ts
+++ src/app/beacon/beacon.page.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectorRef } from "@angular/core";
-import {  Platform } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: "app-beacon",
@@ -8,21 +8,19 @@ import {  Platform } from '@ionic/angular';
 })
 export class BeaconPage  {
   beaconData: any;
-  constructor(private change: ChangeDetectorRef,private platform: Platform) {}
+  constructor(private change: ChangeDetectorRef, private platform: Platform) {}
 
 
-  Buscarbeacons() {
-    this.platform.ready().then(() => {
-      evothings.eddystone.startScan(
-        data => {
-          this.beaconData = data;
-          console.log(this.beaconData);
-          setTimeout(() => {
-            this.change.detectChanges();
-          }, 1000);
-        },
-        error => console.error(error)
-      );
-    });
+  Buscarbeacons(){
+    this.platform.ready().then(() =>{
+      evothings.eddystone.startScan((data) => {
+       this.beaconData=data;
+     
+       setTimeout(()=>{
+         this.change.detectChanges();
+       }, 3000);
+      }, error => console.error(error));
+    })
   }
+
 }
