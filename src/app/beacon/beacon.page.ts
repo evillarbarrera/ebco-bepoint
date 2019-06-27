e03f3893dde5791bf9b1025150a463fb63997c63
evillarbarrera <e.villarbarrera@gmai.com>
2019-06-26 22:43:03 -0400

    data beacons

--- src/app/beacon/beacon.page.ts
+++ src/app/beacon/beacon.page.ts
@@ -9,15 +9,31 @@ import { Platform } from '@ionic/angular';
 })
 export class BeaconPage  {
   beaconData: any;
+  
+  address: string;
+  temperature: string;
+  rssi: string;
+  txPower: string;
+  nombre: string;
+  voltage: string;
+  url: string;
+
   constructor(private change: ChangeDetectorRef, private platform: Platform) {}
 
   Buscarbeacons(){
-    alert("Buscando...")
+   // alert("Buscando...")
     this.platform.ready().then(() =>{
-      alert("Buscando2...")
+     // alert("Buscando2...")
       evothings.eddystone.startScan((data) => {
        this.beaconData=data;
-       alert("Buscando3...," + this.beaconData.address)
+      // alert("Buscando3...," + this.beaconData.address)
+      this.address =  this.beaconData.address;
+      this.temperature = this.beaconData.temperature;
+      this.rssi = this.beaconData.rssi;
+      this.txPower = this.beaconData.txPower;
+      this.nombre = this.beaconData.name;
+      this.voltage = this.beaconData.voltage;
+      this.url = this.beaconData.url;
        setTimeout(()=>{
          this.change.detectChanges();
        }, 1000);
